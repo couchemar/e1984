91f5d373ae37f97602c662e6252c9af37b41b669
Andrey Pavlov <couchemar@yandex.ru>
2012-09-16 17:10:23 +0400

    Добавляем в тестах записи в формате реальной работы но без словаря

--- apps/e1984/test/metrics_store_tests.erl
+++ apps/e1984/test/metrics_store_tests.erl
@@ -10,10 +10,15 @@ start() ->
     ets:new(metrics, [named_table, set]).
 
 store_test(_) ->
-    metrics_store:put(1, 4),
-    metrics_store:put(2, 3),
-    metrics_store:put(3, 2),
-    metrics_store:put(4, 1),
+    metrics_store:put({"Namespace1", "test", "metric1"},
+                      {1, "Count"}),
+    metrics_store:put({"Namespace2", "test", "metric2"},
+                      {2, "Count"}),
+    metrics_store:put({"Namespace1", "test", "metric3"},
+                      {3, "Count"}),
+    metrics_store:put({"Namespace1", "test", "metric4"},
+                      {4, "Count"}),
+
 
     [?_assertEqual(
         [{4, 1}, {3, 2}, {2, 3}, {1, 4}],
