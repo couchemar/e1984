2b16e187cb8e15c9ffe49873ac74175cde022dde
Andrey Pavlov <couchemar@yandex.ru>
2012-09-18 18:28:26 +0400

    Подпилили тесты так что бы получить результат ожидаемый в пушере.

--- apps/e1984/src/metrics_store.erl
+++ apps/e1984/src/metrics_store.erl
@@ -9,6 +9,9 @@
 put(Key, Metric) ->
     ets:insert(?METRICS, {Key, Metric}).
 
+%% @doc: Возвращает все накопленые метрики в виде словаря.
+%% Функция Transform должна возвращать ключ и значение которе
+%% попадет в словарь.
 get_metrics(Transform) ->
     R = qlc:q([Transform(K, V) || {K,V} <- ets:table(?METRICS)]),
     qlc:e(R).
--- apps/e1984/test/metrics_store_tests.erl
+++ apps/e1984/test/metrics_store_tests.erl
@@ -11,14 +11,28 @@ start() ->
 
 store_test(_) ->
     metrics_store:put({"Namespace1", "test", "metric1"},
-                      dict:from_list([{1, "Count"}])),
+                      dict:from_list([{"key1", {1, "Count"}}])),
     metrics_store:put({"Namespace2", "test", "metric2"},
-                      dict:from_list([{2, "Count"}])),
+                      dict:from_list([{"key1", {2, "Count"}}])),
     metrics_store:put({"Namespace1", "test", "metric3"},
-                      dict:from_list([{3, "Count"}])),
+                      dict:from_list([{"key1", {3, "Count"}},
+                                      {"key2", {5, "Count"}}])),
     metrics_store:put({"Namespace1", "test", "metric4"},
-                      dict:from_list([{4, "Count"}])),
+                      dict:from_list([{"key1", {4, "Count"}}])),
 
+    F = fun (Key, Value) ->
+                {Ns, _, M1} = Key,
+                ValueList = dict:to_list(Value),
+                F1 = fun ({K, V}) ->  {M1 ++ "_" ++ K, V} end,
+                ValueList1 = lists:map(F1, ValueList),
+                {Ns, ValueList1}
+        end,
     [?_assertEqual(
-        [{4, 1}, {3, 2}, {2, 3}, {1, 4}],
-        metrics_store:get_metrics(fun (K, V) -> {V+10, K+100} end))].
+        dict:from_list([{"Namespace1", [{"metric1_key1", {1, "Count"}},
+                                        {"metric3_key1", {3, "Count"}},
+                                        {"metric1_key2", {5, "Count"}},
+                                        {"metric4_key1", {4, "Count"}}
+                                       ]},
+                        {"Namespace2", [{"metric2_key1", {2, "Count"}}]}]),
+        metrics_store:get_metrics(F)
+       )].
